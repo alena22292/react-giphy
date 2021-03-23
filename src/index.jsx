3df13f3c6232c040aae7fdea45f4cbebe22a01db
Alena Gritsiuk <alena22292@mail.ru>
2021-03-23 21:23:57 +0100

    Update

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom';
 import '../assets/stylesheets/application.scss';
 import App from './components/app.jsx';
 
-
 const root = document.getElementById('root');
 if (root) {
   ReactDOM.render(
